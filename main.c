dec7eddba7e81f4c73387885e56ce3ac5cda200a
au24032 <au24032-5583@gs4e.chubu.ac.jp>
2026-04-28 12:38:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 
 double a;
   int b;
   
-scanf("%lf", a);
-printf("%d", b);
-printf("%.*f\n", a,b)
+  scanf("%lf", a);
+  printf("%d", b);
+  printf("%.*f\n", a,b)
   return 0;
 }
 
